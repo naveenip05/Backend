97f9e9891f7832b6dc0e4216fd73d689a15f56a2
naveenip05 <115609451+naveenip05@users.noreply.github.com>
2023-05-08 15:53:05 +0530

    Add files via upload
    backend completed

---
+++ EducationLoan/src/main/java/com/example/demo/Repository/AdminRepo.java
@@ -0,0 +1,13 @@
+package com.example.demo.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.Model.AdminModel;
+
+public interface AdminRepo extends JpaRepository<AdminModel,String>{
+
+	void deleteBypassword(String password);
+
+	
+
+}
---
+++ EducationLoan/src/main/java/com/example/demo/Repository/LoanApplicationRepo.java
@@ -0,0 +1,11 @@
+package com.example.demo.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.Model.LoanApplicationModel;
+
+public interface LoanApplicationRepo extends JpaRepository<LoanApplicationModel,Integer> {
+
+	
+
+}
